45af68a33ee438195a59cbb44c3aa945a67958a2
ladovod <ladovod@gmail.com>
2020-04-05 13:24:45 +0500

    добавялет rules для отправки сообщений пользователю при booking lesson

---
+++ sites/all/modules/custom/education_common/templates/education-common-mail-body.tpl.php
@@ -0,0 +1,10 @@
+<span style="font-size:16px">You've made booking №  <?php print $order_num; ?> in http://education.designseonweb.com/<br>
+<br>
+<strong>Contents of booking:&nbsp;</strong><br>
+<u>
+  <?php foreach ($order_items as $order_item): ?>
+    <?php print $order_item['position']; ?>. <?php print $order_item['title']; ?> - <?php print $order_item['quantity']; ?> , summ <?php print $order_item['formatted_summary']; ?>.&nbsp;<br>
+  <?php endforeach; ?>
+</u><br>
+<br>
+
