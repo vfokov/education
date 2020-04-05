2e68892dc602911224b58a36c7c78453b4c9887a
ladovod <ladovod@gmail.com>
2020-04-05 15:25:29 +0500

    adds mime and mailsysytem modules, change rules

--- sites/all/modules/custom/education_common/templates/education-common-mail-body.tpl.php
+++ sites/all/modules/custom/education_common/templates/education-common-mail-body.tpl.php
@@ -1,10 +1,8 @@
 <span style="font-size:16px">You've made booking №  <?php print $order_num; ?> in http://education.designseonweb.com/<br>
 <br>
-<strong>Contents of booking:&nbsp;</strong><br>
-<u>
+<strong>Contents of booking:&nbsp;</strong>
   <?php foreach ($order_items as $order_item): ?>
     <?php print $order_item['position']; ?>. <?php print $order_item['title']; ?> - <?php print $order_item['quantity']; ?> , summ <?php print $order_item['formatted_summary']; ?>.&nbsp;<br>
   <?php endforeach; ?>
-</u><br>
-<br>
+
 
