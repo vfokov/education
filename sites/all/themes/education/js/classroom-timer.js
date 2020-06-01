8aa92e8b6b3651cbe390504b9e32df6ff77ffe98
ladovod <ladovod@gmail.com>
2020-06-01 17:57:42 +0500

    changes for timer

--- sites/all/themes/education/js/classroom-timer.js
+++ sites/all/themes/education/js/classroom-timer.js
@@ -60,8 +60,9 @@
     $('#classroom-timer').countdown('resume');
     var classroom_nid = $('input[name="classroom_nid"]').val();
     var timer_editor_uid = $('input[name="timer_editor_uid"]').val();
+    lock = 1;
     if (editor) {
-      lock = 1;
+
       $.ajax({
         type: 'POST',
         url: '/start-timer',
